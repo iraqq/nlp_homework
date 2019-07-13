2ec751ee51706f548e4ee1b5429ab6806a22e9d3
iraqq <lay3046@sina.com>
2019-07-13 21:58:34 +0800

    Add files via upload

---
+++ syntax_tree.py
@@ -0,0 +1,55 @@
+import random
+
+simple_grammar = """
+sentence => noun_phrase verb_phrase
+noun_phrase => Article Adj* noun
+Adj* => null | Adj Adj*
+verb_phrase => verb noun_phrase
+Article =>  一个 | 这个
+noun =>   女人 |  篮球 | 桌子 | 小猫
+verb => 看着   |  坐在 |  听着 | 看见
+Adj =>  蓝色的 | 好看的 | 小小的
+"""
+
+
+host = """
+host123 = 寒暄 报数 询问 业务相关 结尾 
+报数 = 我是 数字 号 ,
+数字 = 单个数字 | 数字 单个数字 
+单个数字 = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 
+寒暄 = 称谓 打招呼 | 打招呼
+称谓 = 人称 ,
+人称 = 先生 | 女士 | 小朋友
+打招呼 = 你好 | 您好 
+询问 = 请问你要 | 您需要
+业务相关 = 玩玩 具体业务
+玩玩 = null
+具体业务 = 喝酒 | 打牌 | 打猎 | 赌博
+结尾 = 吗？
+"""
+
+def create_grammar(grammar_str, split='=>', line_split='\n'):
+    grammar = {}
+    for line in grammar_str.split(line_split):
+        if not line.strip(): continue
+        exp, stmt = line.split(split)
+        grammar[exp.strip()] = [s.split() for s in stmt.split('|')]
+    return grammar
+
+
+example_grammar = create_grammar(host,split='=')
+
+choice = random.choice
+
+def generate(gram, target):
+    if target not in gram: return target # means target is a terminal expression
+    
+    expaned = [generate(gram, t) for t in choice(gram[target])]
+    return ''.join([e if e != '/n' else '\n' for e in expaned if e != 'null'])
+
+
+#generate(gram=example_grammar, target='host123')
+
+for i in range(20):
+
+    print(generate(gram=create_grammar(host, split='='), target='host1113'))
